4bf81fde506244ba9e1719eb84520ef2466c922d
James Cuzella <james.cuzella@lyraphase.com>
2024-06-15 10:24:55 -0600

    Adding notes about secret confidentiality & destructuring when using dotenv-webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+// destructuring won't work... these will be undefined
+// See: https://github.com/mrsteele/dotenv-webpack/issues/121
 const { TEST, TEST2 } = process.env
 console.log('process.env', process.env)
 console.log('process.env.TEST', process.env.TEST)
+console.log('process.env.TEST2', process.env.TEST2)
+// This secret is NOT secret! It gets hardcoded in the bundled ./dist/main.js
+// Therefore, if your environment variables are secret:
+//   - NEVER check in .env files
+//   - NEVER use dotenv-webpack for secrets in production!
+console.log('process.env.TEST_SECRET', process.env.TEST_SECRET)
 console.log('TEST', TEST)
 console.log('TEST2', TEST2)
+
